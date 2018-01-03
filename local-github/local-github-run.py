3d98e305189940ea70e917bcc3b69670455fef5a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-03 15:47:45 +0000

    Add stub for local Github

--- local-github/local-github-run.py
+++ local-github/local-github-run.py
@@ -4,3 +4,5 @@
 # Create a local folder for storing repos
 # Expose a simple http client that simulates the Github API
 # On repo create, initialise a bare repo with the given path. Return a "file://" url
+
+print "TODO"
